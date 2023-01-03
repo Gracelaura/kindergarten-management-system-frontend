6a308ba47ffc525ee754f0281522a556471f3330
McRonaah <ronaldcheruiyot342@gmail.com>
2023-01-03 13:59:42 +0300

    KIN-8 updated login fields

--- src/App/components/ParentLogin/index.jsx
+++ src/App/components/ParentLogin/index.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 function ParentLogin() {
+  const navigate = useNavigate()
+  // Make sure to install useForm using npm to avoid errors
+  const {register, handleSubmit, formState: {errors}} = useForm()
+  const [login, setLogin] = useState([])
+
+  function onSubmit(data){
+    console.log(data)
+    fetch("http://127.0.0.1:3000/parent_login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res)=> res.json())
+      .then((res)=> setLogin(res), navigate("/parents_dashboard"))
+  }
+
+  console.log(login)
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
     <div className="w-1/3 flex flex-col items-center p-6 bg-white rounded-lg shadow-xl">
@@ -18,7 +40,7 @@ function ParentLogin() {
         placeholder="Password"
         className="px-3 py-2 mb-3 text-gray-700 rounded-lg shadow-sm focus:outline-none focus:shadow-outline w-full"
       />
-      <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full shadow-lg hover:bg-blue-700 focus:outline-none focus:shadow-outline w-full">
+      <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full shadow-lg hover:bg-blue-700 focus:outline-none focus:shadow-outline w-full" onSubmit={handleSubmit(onSubmit)}>
         Log In
       </button>
       <div className="text-center mt-4 text-sm text-gray-600">
