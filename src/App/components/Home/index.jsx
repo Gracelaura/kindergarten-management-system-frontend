de97402ec880dbeb1588a5b7521c7112c766cf9c
McRonaah <ronaldcheruiyot342@gmail.com>
2023-01-03 13:36:16 +0300

    KIN-8 parent login done

--- src/App/components/Home/index.jsx
+++ src/App/components/Home/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+// import ParentLogin from '../ParentLogin'
 
 function Home() {
   return (
-    <div className='bg-red-500 text-5xl grid grid-cols-3'>
+    <div className=''>
         <div className='bg-slate-500 h-11'>one</div>
+        {/* <ParentLogin/> */}
         <div className='bg-yellow-300 h-11'>two</div>
         <div className='bg-black h-11'>three</div>
     </div>
   )
 }
 
-export default Home
+export default Home;
--- src/App/components/ParentLogin/index.jsx
+++ src/App/components/ParentLogin/index.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 function ParentLogin() {
   return (
-    <div>ParentLogin</div>
-  )
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
+    <div className="w-1/3 flex flex-col items-center p-6 bg-white rounded-lg shadow-xl">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqYgAVMzopkLZhO1T-CjbKUHRbq050yn4vtQ&usqp=CAU" alt="Logo" className="h-32 w-32 rounded-full mb-6" />
+      <h1>Parent Login</h1>
+      <input
+        type="text"
+        placeholder="Parent Name"
+        required=""
+        className="px-3 py-2 mb-3 text-gray-700 rounded-lg shadow-sm focus:outline-none focus:shadow-outline w-full"
+      />
+      <input
+        type="password"
+        required=""
+        placeholder="Password"
+        className="px-3 py-2 mb-3 text-gray-700 rounded-lg shadow-sm focus:outline-none focus:shadow-outline w-full"
+      />
+      <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full shadow-lg hover:bg-blue-700 focus:outline-none focus:shadow-outline w-full">
+        Log In
+      </button>
+      <div className="text-center mt-4 text-sm text-gray-600">
+        Don't have an account? <a href="/ParentSignup" className="font-bold text-blue-500 hover:text-blue-800">Sign up</a>
+      </div>
+    </div>
+  </div>
+  );
 }
 
-export default ParentLogin
+export default ParentLogin;
