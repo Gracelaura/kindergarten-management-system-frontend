d1f1328f680946c6aefd54a7488bc4089e8a611f
McRonaah <ronaldcheruiyot342@gmail.com>
2023-01-09 10:27:21 +0300

    KIN-21 added data to be rendered

--- src/App/components/Dashboard/Classes/index.jsx
+++ src/App/components/Dashboard/Classes/index.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import "./Classes.css"
+
+const classesData = [
+  {
+    id: 1,
+    name: 'PP1',
+    teacherName: 'Ms. Smith',
+    numStudents: 20,
+  },
+  {
+    id: 2,
+    name: 'PP2',
+    teacherName: 'Mr. Johnson',
+    numStudents: 22,
+  },
+  {
+    id: 3,
+    name: 'PP3',
+    teacherName: 'Mrs. Williams',
+    numStudents: 19,
+  },
+];
 
 function Classes() {
   return (
-    <div>Classes</div>
+    <div className="classes-page">
+ 
+  </div>
   )
 }
 
