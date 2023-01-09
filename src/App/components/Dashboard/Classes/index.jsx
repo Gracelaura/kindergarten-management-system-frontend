2d0a1cc6820bf1aa6e72edbc8a814ca8e044e07b
McRonaah <ronaldcheruiyot342@gmail.com>
2023-01-09 10:34:06 +0300

    KIN-21 added age range

--- src/App/components/Dashboard/Classes/index.jsx
+++ src/App/components/Dashboard/Classes/index.jsx
@@ -6,18 +6,21 @@ const classesData = [
     id: 1,
     name: 'PP1',
     teacherName: 'Ms. Smith',
+    ageRange: '3-4 years old',
     numStudents: 20,
   },
   {
     id: 2,
     name: 'PP2',
     teacherName: 'Mr. Johnson',
+    ageRange: '4-5 years old',
     numStudents: 22,
   },
   {
     id: 3,
     name: 'PP3',
     teacherName: 'Mrs. Williams',
+    ageRange: '5-6 years old',
     numStudents: 19,
   },
 ];
@@ -30,6 +33,7 @@ function Classes() {
       <div className="class-card" key={klass.id}>
         <h2>{klass.name}</h2>
         <p>Teacher: {klass.teacherName}</p>
+        <p>Age range: {klass.ageRange}</p>
         <p>Number of students: {klass.numStudents}</p>
       </div>
     ))}
