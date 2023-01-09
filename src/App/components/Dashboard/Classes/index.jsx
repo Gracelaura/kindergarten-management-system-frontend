395c29da572635cfbadc63ec79e6faadf9822fad
Muriithi <muriithi.kibata@student.moringaschool.com>
2023-01-09 14:35:42 +0300

    Done

--- src/App/components/Dashboard/Classes/index.jsx
+++ src/App/components/Dashboard/Classes/index.jsx
@@ -1,43 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./Classes.css"
 
-const classesData = [
-  {
-    id: 1,
-    name: 'PP1',
-    teacherName: 'Ms. Smith',
-    ageRange: '3-4 years old',
-    numStudents: 20,
-  },
-  {
-    id: 2,
-    name: 'PP2',
-    teacherName: 'Mr. Johnson',
-    ageRange: '4-5 years old',
-    numStudents: 22,
-  },
-  {
-    id: 3,
-    name: 'PP3',
-    teacherName: 'Mrs. Williams',
-    ageRange: '5-6 years old',
-    numStudents: 19,
-  },
-];
 
-function Classes() {
+
+function Classes(){
+  const [classes, setClasses] = useState()
+  const token = localStorage.getItem("teacherToken")
+  console.log(token)
+
+  function getClasses(){
+    fetch("http://127.0.0.1:3000/classrooms",{
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    }).then((res)=> res.json())
+      .then((data)=> setClasses(data))
+    
+  }
+  console.log(classes)
+  useEffect(()=>{
+  getClasses()
+},[])
   return (
     <div className="classes-page">
     <h1>Classes</h1>
-    {classesData.map(klass => (
+    {classes && classes.map(klass => (
       <div className="class-card" key={klass.id}>
         <h2>{klass.name}</h2>
-        <p>Teacher: {klass.teacherName}</p>
-        <p>Age range: {klass.ageRange}</p>
-        <p>Number of students: {klass.numStudents}</p>
+        <p>Teacher: {klass.teacher.first_name} {klass.teacher.last_name}</p>
+        <button className="button-5" type="submit">See Students</button>
       </div>
     ))}
   </div>
+  
   )
 }
 
