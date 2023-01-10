7e5884fb50067a1bcdef73c4ed93c058245cad30
McRonaah <ronaldcheruiyot342@gmail.com>
2023-01-10 23:17:13 +0300

    KIN-33 created css file and updated an avatar

--- src/App/components/Dashboard/Profile/index.jsx
+++ src/App/components/Dashboard/Profile/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./Profile.css"
+
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div className="container" class='flex flex-col items-center'>
+      <h1>Teacher's Profile</h1>
+    <img src="https://img.freepik.com/free-photo/mother-with-daughter_144627-13199.jpg?w=740&t=st=1673381036~exp=1673381636~hmac=be0eb86900ef6d793a8d3b0e3f6df54fdb7ad78048676e284ae975c66d6f1f4e" alt="Avatar" className="avatar" />
+    <div className="skills">
+      
+    </div>
+  </div>
   )
 }
 
-export default Profile
+export default Profile;
