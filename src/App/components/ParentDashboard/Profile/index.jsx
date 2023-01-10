ee01e654956d476d79adbe81d1a127211ea59006
Gracelaura <gracenjuguna05@gmail.com>
2023-01-10 09:35:47 +0300

    completed adding a form to the parents profile, added styling

--- src/App/components/ParentDashboard/Profile/index.jsx
+++ src/App/components/ParentDashboard/Profile/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import './profile.css'
 
 function Profile() {
   return (
-    <div>Profile</div>
+  <>
+    <div>PARENTS PROFILE.</div>
+
+    <div className='profile'>
+      <h1>Your Profile</h1>
+      <form className='profile-form'>
+          <input class=""
+            type='text'
+            placeholder='Enter Your First Name'
+            name='FirstName'
+          
+          />
+          <input
+            type='text'
+            placeholder='Enter Your Last Name'
+            name='LastName'
+          
+          />
+          <input
+            type='number'
+            placeholder='Enter Your Phone Number'
+            name='phonenumber'
+          
+          /> 
+      </form>
+    </div>
+    
+  </>
+    
+    
   )
 }
 
