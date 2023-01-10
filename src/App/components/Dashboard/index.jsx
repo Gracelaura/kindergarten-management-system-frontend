c088f99b5f99fb9691171970eed61b112dfb5881
Muriithi <muriithi.kibata@student.moringaschool.com>
2023-01-10 15:43:11 +0300

    KIN-35 Done

--- src/App/components/Dashboard/index.jsx
+++ src/App/components/Dashboard/index.jsx
@@ -18,7 +18,37 @@ if(token){
        
     </div>
   )
-}else{}
+}else{
+  return(
+    <div class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+            <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+                <div class="relative">
+                    <div class="absolute">
+                        <div class="">
+                            <h1 class="my-2 text-gray-800 font-bold text-2xl">
+                                Looks like you're lost
+                            </h1>
+                            <p class="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
+                            <button class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-[#B124A3] text-white hover:bg-[#B124A3] focus:outline-none focus:ring-2 focus:ring-[#B124A3] focus:ring-opacity-50">Return to Home!</button>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+                    </div>
+                </div>
+            </div>
+            <div>
+                {/* login sign up */}
+                <div>
+                        <img src="https://media.tenor.com/6rGhXJSw7A8AAAAi/funder-the-sea-octopus.gif" />
+                    </div>
+      
+      {/* login sign up */}
+            </div>
+        </div>
+
+  )
+}
   
 }
 
