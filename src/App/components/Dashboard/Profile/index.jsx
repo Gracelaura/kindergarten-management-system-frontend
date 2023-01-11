67c699ccb4f43b2296455821298f8b9a6c7e6aad
McRonaah <ronaldcheruiyot342@gmail.com>
2023-01-11 19:20:16 +0300

    KIN-33 styling display

--- src/App/components/Dashboard/Profile/index.jsx
+++ src/App/components/Dashboard/Profile/index.jsx
@@ -17,12 +17,12 @@ function Profile() {
 }, []);
 
   return (
-    <div className="container" class='flex flex-col items-center'>
-      <div>
-        <h1>Teacher's Profile</h1>
+    <div className="hero" >
+      <div className='container1'>
+        <h2>Teacher's Profile</h2>
         <img src="https://img.freepik.com/free-photo/mother-with-daughter_144627-13199.jpg?w=740&t=st=1673381036~exp=1673381636~hmac=be0eb86900ef6d793a8d3b0e3f6df54fdb7ad78048676e284ae975c66d6f1f4e" alt="Avatar" className="avatar" />
       </div>
-      <div>
+      <div className='container2'>
       <p>Full Name: {teacherDetails.first_name} {teacherDetails.last_name}</p>
       <p>Career Name: {teacherDetails.career_name}</p>
       <p>Email: {teacherDetails.email}</p>
