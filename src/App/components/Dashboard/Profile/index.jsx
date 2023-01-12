ae4c9b606203cb03b1c8968c07ec27f952364891
McRonaah <ronaldcheruiyot342@gmail.com>
2023-01-12 23:52:34 +0300

    KIN-33 done

--- src/App/components/Dashboard/Profile/index.jsx
+++ src/App/components/Dashboard/Profile/index.jsx
@@ -1,60 +1,44 @@
-import React from 'react'
-import { useState, useEffect } from "react";
-import avatar1 from './images/avatar1.png';
-
-
-import "./Profile.css"
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import avatar from './images/avatar.svg';
+import "./Profile.css";
 
 function Profile() {
-  // const [teacherDetails, setTeacherDetails] = useState({});
- 
-//   useEffect(() => {
+  const navigate = useNavigate()
+  const teacherInfo= localStorage.getItem("teacher_data");
 
-
-// }, []);
-const teacherInfo= localStorage.getItem("teacher_data");
-
-const teacherDetails = JSON.parse(teacherInfo);
-// console.log('teacherDetails', teacherDetails)
-// setTeacherDetails(teacherDetails);
+  const teacherDetails = JSON.parse(teacherInfo);
 
   return (
-    <div className="hero" >
-      {/* <div className='container1'>
-        <h2>Teacher's Profile</h2>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn4PsQCVFAt-oxVMzrHp66Rc7YCvfqXwNKcw&usqp=CAU" alt="Avatar" className="avatar" />
-      </div> */}
-      <div className='container2 flex justify-center items-center'>
-      
-      <form className='bg-[#F9FAFE] p-6 mt-6 rounded-lg shadow-md flex flex-col w-3/5'>
-      {/* {teacherDetails.first_name} {teacherDetails.last_name} */}
-      <p className='text-gray-500'>Full Name: </p>
-     <input className="h-10 rounded-md bg-white text-black italic"
-       value={teacherDetails.teacher.first_name +' ' + teacherDetails.teacher.last_name} 
-     />
-      <p className='text-gray-500'>Career Name:</p>
-     <input className="h-10 rounded-md bg-white text-black italic "
-       value={teacherDetails.teacher.career_name}
-     />
-     <p className='text-gray-500'>Email:</p>
-      <input className="h-10 rounded-md bg-white text-black italic"
-       value={teacherDetails.teacher.email}
-     />
-      <p className='text-gray-500'>Phone Number:</p>
-      <input className="h-10 rounded-md bg-gray text-black italic"
-       value={teacherDetails.teacher.phone_number}
-     />
-           <p className='text-gray-500'>Gender:</p>
-     <input className="h-8 rounded-md bg-white text-black italic"
-       value={teacherDetails.teacher.gender}
-     />
-      <button class="bg-[#B124A3] text-white py-2 px-4 mt-2 rounded-lg" href="#">Back</button>
-     </form>
-      {/* </div> */}
+    <div>
+      <div className='container2 flex justify-center items-center'> 
+        <form className='bg-[#F9FAFE] p-6 mt-6 rounded-lg shadow-md flex flex-col w-3/5'>
+          <h1 class='text-center text-2xl' >Teacher's Profile</h1>
+          <img src={avatar} alt="Avatar" className="avatar object-contain" />
+            <p className='text-gray-500'>Full Name: </p>
+            <input className="h-10 rounded-md bg-white text-black italic text-xl"
+              value={teacherDetails.teacher.first_name +' ' + teacherDetails.teacher.last_name} 
+            />
+            <p className='text-gray-500'>Career Name:</p>
+            <input className="h-10 rounded-md bg-white text-black italic text-xl "
+              value={teacherDetails.teacher.career_name}
+            />
+            <p className='text-gray-500'>Email:</p>
+            <input className="h-10 rounded-md bg-white text-black italic text-xl"
+              value={teacherDetails.teacher.email}
+            />
+            <p className='text-gray-500'>Phone Number:</p>
+            <input className="h-10 rounded-md bg-gray text-black italic text-xl"
+              value={teacherDetails.teacher.phone_number}
+            />
+            <p className='text-gray-500 '>Gender:</p>
+            <input className="h-8 rounded-md bg-white text-black italic text-xl"
+              value={teacherDetails.teacher.gender}
+            />
+          <button onClick={()=>navigate("/dashboard")} class="bg-[#B124A3] text-white py-1 px-4 mt-2 rounded-lg ">Back</button>
+        </form>
       </div>
   </div>
   );
 }
-
 export default Profile;
