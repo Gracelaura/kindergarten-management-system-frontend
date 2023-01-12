046cb2a415f2e54dee318a140cfcaac871dcd863
McRonaah <ronaldcheruiyot342@gmail.com>
2023-01-12 10:04:47 +0300

    KIN-33 styling profile and update font size

--- src/App/components/Dashboard/Profile/index.jsx
+++ src/App/components/Dashboard/Profile/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from "react";
+import avatar1 from './images/avatar1.png';
+
 
 import "./Profile.css"
 
@@ -20,14 +22,18 @@ function Profile() {
     <div className="hero" >
       <div className='container1'>
         <h2>Teacher's Profile</h2>
-        <img src="https://img.freepik.com/free-photo/mother-with-daughter_144627-13199.jpg?w=740&t=st=1673381036~exp=1673381636~hmac=be0eb86900ef6d793a8d3b0e3f6df54fdb7ad78048676e284ae975c66d6f1f4e" alt="Avatar" className="avatar" />
+        {/* https://img.freepik.com/free-photo/mother-with-daughter_144627-13199.jpg?w=740&t=st=1673381036~exp=1673381636~hmac=be0eb86900ef6d793a8d3b0e3f6df54fdb7ad78048676e284ae975c66d6f1f4e */}
+        <img src={avatar1} alt="Avatar" className="avatar" />
       </div>
       <div className='container2'>
       <p>Full Name: {teacherDetails.first_name} {teacherDetails.last_name}</p>
       <p>Career Name: {teacherDetails.career_name}</p>
       <p>Email: {teacherDetails.email}</p>
+      <div className='container3'>
       <p>Phone Number: {teacherDetails.phone_number}</p>
       <p>Gender: {teacherDetails.gender}</p>
+      {/* <h1>Other Profiles</h1> */}
+      </div>
       </div>
   </div>
   );
