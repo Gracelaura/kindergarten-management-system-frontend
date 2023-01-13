1543cb3ec2d25eab3b613951446869c91f126d84
Gracelaura <gracenjuguna05@gmail.com>
2023-01-13 14:28:55 +0300

    KIN-55 modified styling

--- src/App/components/ParentDashboard/Welcome/index.jsx
+++ src/App/components/ParentDashboard/Welcome/index.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
-import './welcome.css'
-import ParentsKids from './assets/image/parents-kids.png'
+import icon from './icon.svg'
+import screens from './screens.svg'
 
 function Welcome() {
   return (
   <>
-    
-    <div className='main'>
+  <div className='flex justify-center'>
+    <h1 className='text-[#B124A3] text-xl font-semibold'>Parent Dashboard</h1>
+  </div>
 
-    <div className="content">
-        <h2>WELCOME TO KINDERJOY</h2>
-        <p>We believe there is nothing more important than education.</p>
-      </div>
-     
-      <div className="image-card">
-        <img src={ParentsKids} alt="parents and kids"/>
-      </div>
+  <div className="h-48 m-auto w-4/5 bg-[#B124A3] rounded-md flex flex-row justify-between">
+        <div className="m-auto p-5 h-full w-1/2 text-white">
+            <div>kindergarten management portal</div>
+            <h1 className="text-5xl">Welcome Parent</h1>
+            <p>Hello Parent</p>
+        </div>
+        <div className="bg-white rounded-full h-24 w-24 m-auto flex justify-center items-center">
+            <img  src={icon} alt="icon"/>
+        </div>
+  </div>
+
+  <div className="grid grid-cols-1 pt-14">
+        <div className="flex justify-center">
+            <img src={screens} alt="screens"/>
+        </div>
+  </div>      
       
-      
-    </div>
-   
+ 
   </>
     
   )
