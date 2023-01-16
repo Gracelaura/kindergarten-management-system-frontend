a830127b039d43676bf4fc792a7ac851628352b3
Timothy Kibet <tkkibet13@gmail.com>
2023-01-16 18:24:36 +0300

    KIN-49 focus and clean up

--- src/App/components/Home/Nav.jsx
+++ src/App/components/Home/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../Home/assets/pre-logo.png";
-// import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
 
 const Navbar = () => {
   const [nav, setNav] = useState(true);
@@ -34,17 +34,6 @@ const navigate = useNavigate()
         <li onClick={()=>navigate("/login")} className="p-4 cursor-pointer">Teacher Login</li>
         <li onClick={()=>navigate("/signup")} className="p-4 cursor-pointer">Teacher Signup</li>
       </ul>
-      {/* <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
-      </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-white-900 bg-white ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#B124A3] m-4'>KinderJoy.</h1>
-          <li className='p-4 border-b '>Home</li>
-          <li className='p-4 border-b '>Login</li>
-          <li className='p-4 border-b '>Register</li>
-          <li className='p-4 border-b '>Help</li>
-          
-      </ul> */}
     </div>
   );
 };
